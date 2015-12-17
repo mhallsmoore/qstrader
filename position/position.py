fbddabcb06b24cce62eb70d55448e63e73bc9e8c
Michael Halls-Moore <mike@quarkgluon.com>
2015-12-17 14:56:46 +0000

    Overhauled Position handling to mirror how Interactive Brokers handles positions, using Buys/Sells, Net, Avg_BOT, Avg_SLD and Net Incl Commission. This allows calculation of Realised and Unrealised PnL. Two roundtrip unit tests added to test calculations.

--- position/position.py
+++ position/position.py
@@ -7,69 +7,116 @@ FIVEPLACES = Decimal("0.00001")
 
 
 class Position(object):
-    def __init__(self, fill, bid, ask):
-        self.ticker = fill.ticker
-        self.side = fill.side
-        self.quantity = fill.quantity
-
-        # Calculate initial values and update prices
-        self._calculate_initial_value(fill)
-        self.update_position_prices(bid, ask)
+    def __init__(
+        self, action, ticker, init_quantity, 
+        init_price, init_commission, 
+        bid, ask
+    ):
+        self.action = action
+        self.ticker = ticker
+        self.quantity = init_quantity
+        self.init_price = init_price
+        self.init_commission = init_commission
         
-    def _calculate_initial_value(self, fill):
-        self.avg_price = (
-            (fill.price * fill.quantity + fill.commission)/self.quantity
-        ).quantize(FIVEPLACES)
-        self.cost_basis = (
-            self.quantity * self.avg_price
-        ).quantize(TWOPLACES)
+        self.realised_pnl = Decimal("0.00")
+        self.unrealised_pnl = Decimal("0.00")
 
-    def update_position_prices(self, bid, ask):
-        # Update market bid and ask price
-        self.bid = bid
-        self.ask = ask
+        self.buys = Decimal("0")
+        self.sells = Decimal("0")
+        self.avg_bot = Decimal("0.00")
+        self.avg_sld = Decimal("0.00")
+        self.total_bot = Decimal("0.00")
+        self.total_sld = Decimal("0.00")
+        self.total_commission = init_commission
 
-        # Calculate market price depending on trade side
-        if self.side == "LONG":
-            self.market_price = self.bid
-        else:
-            self.market_price = self.ask
+        self._calculate_initial_value()
+        self.update_market_value(bid, ask)
 
-        # Calculate market value and unrealised profit & loss
+    def _calculate_initial_value(self):
+        if self.action == "BOT":
+            self.buys = self.quantity
+            self.avg_bot = self.init_price.quantize(FIVEPLACES)
+            self.total_bot = (self.buys * self.avg_bot).quantize(TWOPLACES)
+            self.avg_price = (
+                (self.init_price * self.quantity + self.init_commission)/self.quantity
+            ).quantize(FIVEPLACES)
+            self.cost_basis = (
+                self.quantity * self.avg_price
+            ).quantize(TWOPLACES)
+        else:  # action == "SLD"
+            self.sells = self.quantity
+            self.avg_sld = self.init_price.quantize(FIVEPLACES)
+            self.total_sld = (self.sells * self.avg_sld).quantize(TWOPLACES)
+            self.avg_price = (
+                (self.init_price * self.quantity - self.init_commission)/self.quantity
+            ).quantize(FIVEPLACES)
+            self.cost_basis = (
+                -self.quantity * self.avg_price
+            ).quantize(TWOPLACES)
+        self.net = self.buys - self.sells
+        self.net_total = (self.total_sld - self.total_bot).quantize(TWOPLACES)
+        self.net_incl_comm = (self.net_total - self.init_commission).quantize(TWOPLACES)
+
+    def update_market_value(self, bid, ask):
+        midpoint = (bid+ask)/Decimal("2.0")
         self.market_value = (
-            self.quantity * self.market_price
+            self.quantity * midpoint
         ).quantize(TWOPLACES)
-        self.abs_unrealized_pnl = (
+        self.unrealised_pnl = (
             self.market_value - self.cost_basis
         ).quantize(TWOPLACES)
-        self.per_unrealized_pnl = (
-            (
-                self.abs_unrealized_pnl/self.cost_basis
-            ).quantize(FIVEPLACES) * Decimal("100.00")
+        self.realised_pnl = (
+            self.market_value + self.net_incl_comm
+        )
+
+    def transact_shares(self, action, quantity, price, commission):
+        prev_quantity = self.quantity
+        prev_commission = self.total_commission
+
+        self.total_commission += commission
+        
+        # Adjust total bought and sold
+        if action == "BOT":            
+            self.avg_bot = (
+                (self.avg_bot*self.buys + price*quantity)/(self.buys + quantity)
+            ).quantize(FIVEPLACES)
+            if self.action != "SLD":
+                self.avg_price = (
+                    (
+                        self.avg_price*self.buys + 
+                        price*quantity+commission
+                    )/(self.buys + quantity)
+                ).quantize(FIVEPLACES)
+            self.buys += quantity
+            self.total_bot = (self.buys * self.avg_bot).quantize(TWOPLACES)
+
+        # action == "SLD"
+        else:  
+            self.avg_sld = (
+                (self.avg_sld*self.sells + price*quantity)/(self.sells + quantity)
+            ).quantize(FIVEPLACES)
+            if self.action != "BOT":
+                self.avg_price = (
+                    (
+                        self.avg_price*self.sells + 
+                        price*quantity-commission
+                    )/(self.sells + quantity)
+                ).quantize(FIVEPLACES)
+            self.sells += quantity
+            self.total_sld = (self.sells * self.avg_sld).quantize(TWOPLACES)
+
+        # Adjust net values, including commissions
+        self.net = self.buys - self.sells
+        
+        self.quantity = self.net
+        self.net_total = (
+            self.total_sld - self.total_bot
+        ).quantize(TWOPLACES)
+        self.net_incl_comm = (
+            self.net_total - self.total_commission
         ).quantize(TWOPLACES)
 
-    def buy_shares(self, fill, bid, ask):
-        old_quantity = copy.deepcopy(self.quantity)
-        self.quantity += fill.quantity
-        old_cost_basis = self.avg_price * old_quantity
-        new_cost_basis = fill.price * fill.quantity + fill.commission
-
-        # Update average price and cost basis
-        self.avg_price = (
-            (old_cost_basis + new_cost_basis)/self.quantity
-        ).quantize(TWOPLACES)
+        # Adjust average price and cost basis
         self.cost_basis = (
             self.quantity * self.avg_price
         ).quantize(TWOPLACES)
-
-        # Update market value and PnL
-        self.update_position_prices(bid, ask)
-
-    """
-    def sell_shares(self, fill, bid, ask):
-        pass
-
-    def close_position(self, fill):
-        pass
-    """
-    
--- position/position_test.py
+++ position/position_test.py
@@ -4,254 +4,94 @@ import unittest
 from position import Position
 
 
-class FillEventMock(object):
-    """
-    A mock object that allows a representation
-    of a FillEvent object
-    """
-    def __init__(
-        self, ticker, side, 
-        quantity, price, commission
-    ):
-        self.ticker = ticker
-        self.side = side
-        self.quantity = quantity
-        self.price = price
-        self.commission = commission
-
-
-class TestLongAAPLPosition(unittest.TestCase):
-    """
-    AAPL longed at 100 shares at ask of $118.12
-    with brokerage commission at $1.00 
-    """
+class TestRoundTripXOMPosition(unittest.TestCase):
     def setUp(self):
-        """
-        Set up the FillEvent and Position object
-        to reflect a LONG AAPL open position
-        """
-        bid = Decimal("118.11")
-        ask = Decimal("118.12")
-        fill = FillEventMock(
-            "AAPL", "LONG", 100, 
-            ask, Decimal("1.00")
+        self.position = Position(
+            "BOT", "XOM", Decimal('100'), 
+            Decimal("74.78"), Decimal("1.00"), 
+            Decimal('74.78'), Decimal('74.80')
         )
-        self.position = Position(fill, bid, ask)
 
-    def test_calculate_initial_values(self):
-        """
-        Test the calculation of the initial values
-        of the Position object
-        """
-        self.assertEqual(self.position.ticker, "AAPL")
-        self.assertEqual(self.position.side, "LONG")
-        self.assertEqual(self.position.quantity, 100)
-        self.assertEqual(self.position.avg_price, Decimal("118.13000"))
-        self.assertEqual(self.position.cost_basis, Decimal("11813.00"))
-        self.assertEqual(self.position.bid, Decimal("118.11"))
-        self.assertEqual(self.position.ask, Decimal("118.12"))
-        self.assertEqual(self.position.market_price, Decimal("118.11"))
-        self.assertEqual(self.position.market_value, Decimal("11811.00"))
-        self.assertEqual(self.position.abs_unrealized_pnl, Decimal("-2.00"))
-        self.assertEqual(self.position.per_unrealized_pnl, Decimal("-0.02"))
+    def test_calculate_round_trip(self):
+        self.position.transact_shares(
+            "BOT", Decimal('100'), Decimal('74.63'), Decimal('1.00')
+        )
+        self.position.transact_shares(
+            "BOT", Decimal('250'), Decimal('74.620'), Decimal('1.25')
+        )
+        self.position.transact_shares(
+            "SLD", Decimal('200'), Decimal('74.58'), Decimal('1.00')
+        )
+        self.position.transact_shares(
+            "SLD", Decimal('250'), Decimal('75.26'), Decimal('1.25')
+        )
+        self.position.update_market_value(Decimal("77.75"), Decimal("77.77"))
 
-    def test_update_position_prices(self):
-        """
-        Update AAPL's bid and ask prices, then check that
-        the Position correctly updates it prices
-        """
-        bid = Decimal("117.43")
-        ask = Decimal("117.44")
-        self.position.update_position_prices(bid, ask)
-        self.assertEqual(self.position.bid, Decimal("117.43"))
-        self.assertEqual(self.position.ask, Decimal("117.44"))
-        self.assertEqual(self.position.market_price, Decimal("117.43"))
-        self.assertEqual(self.position.market_value, Decimal('11743.00'))
-        self.assertEqual(self.position.abs_unrealized_pnl, Decimal("-70.00"))
-        self.assertEqual(self.position.per_unrealized_pnl, Decimal("-0.59"))
+        self.assertEqual(self.position.action, "BOT")
+        self.assertEqual(self.position.ticker, "XOM")
+        self.assertEqual(self.position.quantity, Decimal("0"))
+        
+        self.assertEqual(self.position.buys, Decimal("450"))
+        self.assertEqual(self.position.sells, Decimal("450"))
+        self.assertEqual(self.position.net, Decimal("0"))
+        self.assertEqual(self.position.avg_bot, Decimal("74.65778"))
+        self.assertEqual(self.position.avg_sld, Decimal("74.95778"))
+        self.assertEqual(self.position.total_bot, Decimal("33596.00"))
+        self.assertEqual(self.position.total_sld, Decimal("33731.00"))
+        self.assertEqual(self.position.net_total, Decimal("135.00"))
+        self.assertEqual(self.position.total_commission, Decimal("5.50"))
+        self.assertEqual(self.position.net_incl_comm, Decimal("129.50"))
+
+        self.assertEqual(self.position.avg_price, Decimal("74.665"))
+        self.assertEqual(self.position.cost_basis, Decimal("0.00"))
+        self.assertEqual(self.position.market_value, Decimal("0.00"))
+        self.assertEqual(self.position.unrealised_pnl, Decimal("0.00"))
+        self.assertEqual(self.position.realised_pnl, Decimal("129.50"))
 
 
-class TestLongDGEPosition(unittest.TestCase):
-    """
-    DGE longed at 52 shares at ask of £18.87
-    with brokerage commission at £8.86 
-    """
+class TestRoundTripPGPosition(unittest.TestCase):
     def setUp(self):
-        """
-        Set up the FillEvent and Position object
-        to reflect a LONG DGE open position
-        """
-        bid = Decimal("18.85315")
-        ask = Decimal("18.87315")
-        fill = FillEventMock(
-            "DGE", "LONG", 52, 
-            ask, Decimal("8.86")
+        self.position = Position(
+            "SLD", "PG", Decimal('100'), 
+            Decimal("77.69"), Decimal("1.00"), 
+            Decimal('77.68'), Decimal('77.70')
         )
-        self.position = Position(fill, bid, ask)
 
-    def test_calculate_initial_values(self):
-        """
-        Test the calculation of the initial values
-        of the Position object
-        """
-        self.assertEqual(self.position.ticker, "DGE")
-        self.assertEqual(self.position.side, "LONG")
-        self.assertEqual(self.position.quantity, 52)
-        self.assertEqual(self.position.avg_price, Decimal("19.04353"))
-        self.assertEqual(self.position.cost_basis, Decimal("990.26"))
-        self.assertEqual(self.position.bid, Decimal("18.85315"))
-        self.assertEqual(self.position.ask, Decimal("18.87315"))
-        self.assertEqual(self.position.market_price, Decimal("18.85315"))
-        self.assertEqual(self.position.market_value, Decimal("980.36"))
-        self.assertEqual(self.position.abs_unrealized_pnl, Decimal("-9.90"))
-        self.assertEqual(self.position.per_unrealized_pnl, Decimal("-1.00"))
-
-    def test_update_position_prices(self):
-        """
-        Update DGE's bid and ask prices, then check that
-        the Position correctly updates it prices
-        """
-        bid = Decimal("19.12500")
-        ask = Decimal("19.13500")
-        self.position.update_position_prices(bid, ask)
-        self.assertEqual(self.position.bid, Decimal("19.12500"))
-        self.assertEqual(self.position.ask, Decimal("19.13500"))
-        self.assertEqual(self.position.market_price, Decimal("19.12500"))
-        self.assertEqual(self.position.market_value, Decimal("994.50"))
-        self.assertEqual(self.position.abs_unrealized_pnl, Decimal("4.24"))
-        self.assertEqual(self.position.per_unrealized_pnl, Decimal("0.43"))
-
-
-class TestLongRDSBPosition(unittest.TestCase):
-    """
-    RDSB longed at 53 shares at ask of £18.43
-    with brokerage commission at £8.86 
-    """
-    def setUp(self):
-        """
-        Set up the FillEvent and Position object
-        to reflect a LONG RDSB open position
-        """
-        bid = Decimal("18.4171")
-        ask = Decimal("18.4271")
-        fill = FillEventMock(
-            "RDSB", "LONG", 53, 
-            ask, Decimal("8.83")
+    def test_calculate_round_trip(self):
+        self.position.transact_shares(
+            "SLD", Decimal('100'), Decimal('77.68'), Decimal('1.00')
         )
-        self.position = Position(fill, bid, ask)
-
-    def test_calculate_initial_values(self):
-        """
-        Test the calculation of the initial values
-        of the Position object
-        """
-        self.assertEqual(self.position.ticker, "RDSB")
-        self.assertEqual(self.position.side, "LONG")
-        self.assertEqual(self.position.quantity, 53)
-        self.assertEqual(self.position.avg_price, Decimal("18.59370"))
-        self.assertEqual(self.position.cost_basis, Decimal("985.47"))
-        self.assertEqual(self.position.bid, Decimal("18.4171"))
-        self.assertEqual(self.position.ask, Decimal("18.4271"))
-        self.assertEqual(self.position.market_price, Decimal("18.4171"))
-        self.assertEqual(self.position.market_value, Decimal("976.11"))
-        self.assertEqual(self.position.abs_unrealized_pnl, Decimal("-9.36"))
-        self.assertEqual(self.position.per_unrealized_pnl, Decimal("-0.95"))
-
-    def test_update_position_prices(self):
-        """
-        Update RDSB's bid and ask prices, then check that
-        the Position correctly updates it prices
-        """
-        bid = Decimal("16.41500")
-        ask = Decimal("16.42500")
-        self.position.update_position_prices(bid, ask)
-        self.assertEqual(self.position.bid, Decimal("16.41500"))
-        self.assertEqual(self.position.ask, Decimal("16.42500"))
-        self.assertEqual(self.position.market_price, Decimal("16.41500"))
-        self.assertEqual(self.position.market_value, Decimal("870.00"))
-        self.assertEqual(self.position.abs_unrealized_pnl, Decimal("-115.47"))
-        self.assertEqual(self.position.per_unrealized_pnl, Decimal("-11.72"))
-
-
-class TestLongGOOGPosition(unittest.TestCase):
-    """
-    GOOG longed at 100 shares at ask of $749.90
-    with brokerage commission of $1.00, then a further
-    200 shares longed at ask of $749.485, with brokerage
-    commission of $1.00
-    """
-    def setUp(self):
-        """
-        Set up the FillEvent and Position object
-        to reflect a LONG GOOG open position
-        """
-        init_bid = Decimal("748.57")
-        init_ask = Decimal("749.90")
-        init_fill = FillEventMock(
-            "GOOG", "LONG", 100, 
-            init_ask, Decimal("1.00")
+        self.position.transact_shares(
+            "SLD", Decimal('50'), Decimal('77.70'), Decimal('1.00')
         )
-        self.position = Position(init_fill, init_bid, init_ask)
-
-    def test_calculate_initial_values(self):
-        """
-        Test the calculation of the initial values
-        of the Position object
-        """
-        self.assertEqual(self.position.ticker, "GOOG")
-        self.assertEqual(self.position.side, "LONG")
-        self.assertEqual(self.position.quantity, 100)
-        self.assertEqual(self.position.avg_price, Decimal("749.91"))
-        self.assertEqual(self.position.cost_basis, Decimal("74991.00"))
-        self.assertEqual(self.position.bid, Decimal("748.57"))
-        self.assertEqual(self.position.ask, Decimal("749.90"))
-        self.assertEqual(self.position.market_price, Decimal("748.57"))
-        self.assertEqual(self.position.market_value, Decimal("74857.00"))
-        self.assertEqual(self.position.abs_unrealized_pnl, Decimal("-134.00"))
-        self.assertEqual(self.position.per_unrealized_pnl, Decimal("-0.18"))
-
-    def test_buy_shares_and_price_updates(self):
-        """
-        Tests the ability to buy/long new shares for the Position.
-        Adds another 200 shares of GOOG to the Position at a new
-        market price. Then test for a new bid/ask price and updated
-        market value.
-        """
-        new_bid = Decimal("748.887")
-        new_ask = Decimal("749.485")
-        new_fill = FillEventMock(
-            "GOOG", "LONG", 200,
-            new_ask, Decimal("1.00")
+        self.position.transact_shares(
+            "BOT", Decimal('100'), Decimal('77.77'), Decimal('1.00')
         )
-        self.position.buy_shares(new_fill, new_bid, new_ask)
+        self.position.transact_shares(
+            "BOT", Decimal('150'), Decimal('77.73'), Decimal('1.00')
+        )
+        self.position.update_market_value(Decimal("77.72"), Decimal("77.72"))
 
-        # Test the properties subsequent to the 
-        # "buy_shares" method call
-        self.assertEqual(self.position.ticker, "GOOG")
-        self.assertEqual(self.position.side, "LONG")
-        self.assertEqual(self.position.quantity, 300)
-        self.assertEqual(self.position.avg_price, Decimal("749.63"))
-        self.assertEqual(self.position.cost_basis, Decimal("224889.00"))
-        self.assertEqual(self.position.bid, Decimal("748.887"))
-        self.assertEqual(self.position.ask, Decimal("749.485"))
-        self.assertEqual(self.position.market_price, Decimal("748.887"))
-        self.assertEqual(self.position.market_value, Decimal("224666.10"))
-        self.assertEqual(self.position.abs_unrealized_pnl, Decimal("-222.90"))
-        self.assertEqual(self.position.per_unrealized_pnl, Decimal("-0.10"))
+        self.assertEqual(self.position.action, "SLD")
+        self.assertEqual(self.position.ticker, "PG")
+        self.assertEqual(self.position.quantity, Decimal("0"))
+        
+        self.assertEqual(self.position.buys, Decimal("250"))
+        self.assertEqual(self.position.sells, Decimal("250"))
+        self.assertEqual(self.position.net, Decimal("0"))
+        self.assertEqual(self.position.avg_bot, Decimal("77.746"))
+        self.assertEqual(self.position.avg_sld, Decimal("77.688"))
+        self.assertEqual(self.position.total_bot, Decimal("19436.50"))
+        self.assertEqual(self.position.total_sld, Decimal("19422.00"))
+        self.assertEqual(self.position.net_total, Decimal("-14.50"))
+        self.assertEqual(self.position.total_commission, Decimal("5.00"))
+        self.assertEqual(self.position.net_incl_comm, Decimal("-19.50"))
 
-        # Update the bid and ask prices
-        bid = Decimal("750.95")
-        ask = Decimal("751.57")
-        self.position.update_position_prices(bid, ask)
-        self.assertEqual(self.position.bid, Decimal("750.95"))
-        self.assertEqual(self.position.ask, Decimal("751.57"))
-        # Last Price (used by IB to calculate market price)
-        bid = Decimal("751.57")
-        self.position.update_position_prices(bid, ask)
-        self.assertEqual(self.position.market_price, Decimal("751.57"))
-        self.assertEqual(self.position.market_value, Decimal("225471.00"))
-        self.assertEqual(self.position.abs_unrealized_pnl, Decimal("582.00"))
-        self.assertEqual(self.position.per_unrealized_pnl, Decimal("0.26"))
+        self.assertEqual(self.position.avg_price, Decimal("77.67600"))
+        self.assertEqual(self.position.cost_basis, Decimal("0.00"))
+        self.assertEqual(self.position.market_value, Decimal("0.00"))
+        self.assertEqual(self.position.unrealised_pnl, Decimal("0.00"))
+        self.assertEqual(self.position.realised_pnl, Decimal("-19.50"))
 
 
 if __name__ == "__main__":
