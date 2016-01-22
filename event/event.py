91cea3c5b8341b9f667331fd6ccee630a21cdf95
Michael Halls-Moore <mike@quarkgluon.com>
2016-01-22 11:50:52 +0000

    Added Event objects: Market, Signal, Order and Fill.

---
+++ event/event.py
@@ -0,0 +1,110 @@
+from __future__ import print_function
+
+from abc import ABCMeta, abstractmethod
+
+
+class Event(object):
+    """
+    Event is base class providing an interface for all subsequent 
+    (inherited) events, that will trigger further events in the 
+    trading infrastructure.   
+    """
+    pass
+
+
+class MarketEvent(Event):
+    """
+    Handles the event of receiving a new market update with 
+    corresponding bars.
+    """
+    def __init__(self):
+        """
+        Initialises the MarketEvent.
+        """
+        self.type = 'MARKET'
+
+
+class SignalEvent(Event):
+    """
+    Handles the event of sending a Signal from a Strategy object.
+    This is received by a Portfolio object and acted upon.
+    """  
+    def __init__(self, ticker, action):
+        """
+        Initialises the SignalEvent.
+
+        Parameters:
+        ticker - The ticker symbol, e.g. 'GOOG'.
+        action - 'BOT' (for long) or 'SLD' (for short).
+        """
+        self.type = 'SIGNAL'
+        self.ticker = ticker
+        self.action = action
+
+
+class OrderEvent(Event):
+    """
+    Handles the event of sending an Order to an execution system.
+    The order contains a ticker (e.g. GOOG), action (BOT or SLD)
+    and quantity.
+    """
+    def __init__(self, ticker, action, quantity):
+        """
+        Initialises the OrderEvent.
+
+        Parameters:
+        ticker - The ticker symbol, e.g. 'GOOG'.
+        action - 'BOT' (for long) or 'SLD' (for short).
+        quantity - The quantity of shares to transact.
+        """
+        self.type = 'ORDER'
+        self.ticker = ticker
+        self.action = action
+        self.quantity = quantity
+
+    def print_order(self):
+        """
+        Outputs the values within the OrderEvent.
+        """
+        print(
+            "Order: Ticker=%s, Action=%s, Quantity=%s" % (
+                self.ticker, self.action, self.quantity
+            )
+        )
+
+
+class FillEvent(Event):
+    """
+    Encapsulates the notion of a filled order, as returned
+    from a brokerage. Stores the quantity of an instrument
+    actually filled and at what price. In addition, stores
+    the commission of the trade from the brokerage.
+    
+    TODO: Currently does not support filling positions at
+    different prices. This will be simulated by averaging
+    the cost.
+    """
+
+    def __init__(
+        self, timestamp, ticker, action, quantity, 
+        exchange, price, commission
+    ):
+        """
+        Initialises the FillEvent object. 
+
+        timestamp - The timestamp when the order was filled.
+        ticker - The ticker symbol, e.g. 'GOOG'.
+        action - 'BOT' (for long) or 'SLD' (for short).
+        quantity - The filled quantity.
+        exchange - The exchange where the order was filled.
+        price - The price at which the trade was filled
+        commission - The brokerage commission for carrying out the trade.
+        """
+        self.type = 'FILL'
+        self.timeindex = timeindex
+        self.ticker = ticker
+        self.action = action
+        self.quantity = quantity
+        self.exchange = exchange
+        self.price = price
+        self.commission = commission
