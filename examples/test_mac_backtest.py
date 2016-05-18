5e36752b200d4dcc9a5faf39e1e19a5ef20926b6
Ryan Kennedy <hello@ryankennedy.io>
2016-05-18 13:00:17 +0930

    Use the Test Risk Manager and Position Sizer classes in the example MAC backtest

--- examples/test_mac_backtest.py
+++ examples/test_mac_backtest.py
@@ -3,28 +3,27 @@ from decimal import Decimal
 from qstrader.backtest.backtest import Backtest
 from qstrader.execution_handler.execution_handler import IBSimulatedExecutionHandler
 from qstrader.portfolio_handler.portfolio_handler import PortfolioHandler
-from qstrader.position_sizer.balanced_position_sizer import BalancedPositionSizer
+from qstrader.position_sizer.position_sizer import TestPositionSizer
 from qstrader.price_handler.yahoo_price_handler import YahooDailyBarPriceHandler
-from qstrader.risk_manager.balanced_risk_manager import BalancedRiskManager
+from qstrader.risk_manager.risk_manager import TestRiskManager
 from qstrader.statistics.statistics import SimpleStatistics
 from qstrader import settings
 from qstrader.strategy.moving_average_cross_strategy import MovingAverageCrossStrategy
 try:
-		import Queue as queue
+        import Queue as queue
 except ImportError:
-		import queue
+        import queue
 
 
 if __name__ == "__main__":
     tickers = ["AAPL"]
 
-	# Set up variables needed for backtest
+    # Set up variables needed for backtest
     events_queue = queue.Queue()
     csv_dir = settings.CSV_DATA_DIR
     initial_equity = Decimal("500000.00")
     heartbeat = 0.0
     max_iters = 10000000000
-    NUM_STOCKS = 1
 
     # Use Yahoo Daily Price Handler
     price_handler = YahooDailyBarPriceHandler(
@@ -34,11 +33,11 @@ if __name__ == "__main__":
     # Use the MAC Strategy
     strategy = MovingAverageCrossStrategy( tickers, events_queue )
 
-    # Use an example Position Sizer, 1 stock
-    position_sizer = BalancedPositionSizer(NUM_STOCKS)
+    # Use an example Position Sizer, 
+    position_sizer = TestPositionSizer()
 
-    # Use a balanced Risk Manager, 1 stock
-    risk_manager = BalancedRiskManager(NUM_STOCKS)
+    # Use an example Risk Manager, 
+    risk_manager = TestRiskManager()
 
     # Use the default Portfolio Handler
     portfolio_handler = PortfolioHandler(
