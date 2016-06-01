58e31772dfd69bd0c0ab81673f7774819f41a2b8
Ryan Kennedy <hello@ryankennedy.io>
2016-06-01 20:36:04 +0930

    Compliance module now keeps a CSV trade log of any trades that are executed

--- compliance/compliance.py
+++ compliance/compliance.py
@@ -1,5 +1,9 @@
 from abc import ABCMeta, abstractmethod
 
+import datetime
+import os
+import csv
+from qstrader import settings
 from qstrader.event.event import FillEvent;
 
 class Compliance(object):
@@ -35,8 +39,42 @@ class TestCompliance(Compliance):
     """
 
     def __init__(self):
-        pass
+        """
+        Wipe the existing trade log for the day, leaving only
+        the headers in an empty CSV.
+
+        It allows for multiple backtests to be run
+        in a simple way, but quite likely makes it unsuitable for
+        a production environment that requires strict record-keeping.
+        """
+        # Remove the previous CSV file
+        self.csv_filename = "tradelog_" + datetime.datetime.today().strftime("%Y-%m-%d")
+        try:
+            os.remove(os.path.join(settings.OUTPUT_DIR, self.csv_filename))
+        except FileNotFoundError:
+            print("No tradelog files to clean.");
+
+        # Write new file header
+        fieldnames = [
+            "timestamp", "ticker",
+            "action", "quantity",
+            "exchange", "price",
+            "commission"
+        ]
+        with open(os.path.join(settings.OUTPUT_DIR, self.csv_filename), 'a') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
 
     def record_trade(self, fill):
-        pass
-        # TODO implement.
+        """
+        Append all details about the FillEvent to the CSV trade log.
+        """
+        with open(os.path.join(settings.OUTPUT_DIR, self.csv_filename), 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([
+                fill.timestamp, fill.ticker,
+                fill.action, fill.quantity,
+                fill.exchange, fill.price,
+                fill.commission
+            ])
