1fd72aafccf0865267202e288d7ade7bd50e1ae5
Ryan Kennedy <hello@ryankennedy.io>
2017-03-15 11:07:58 +1100

    Add displayStrategy for live trading example to see market data coming in

--- examples/buy_and_hold_live_ib.py
+++ examples/buy_and_hold_live_ib.py
@@ -1,13 +1,43 @@
 import datetime
 
+from qstrader.price_parser import PriceParser
 from qstrader import settings
-from qstrader.strategy.base import AbstractStrategy
+from qstrader.strategy.base import Strategies, AbstractStrategy
 from qstrader.event import SignalEvent, EventType
 from qstrader.compat import queue
 from qstrader.trading_session import TradingSession
 from qstrader.service.ib import IBService
 from qstrader.price_handler.ib_bar import IBBarPriceHandler
 from ibapi.contract import Contract
+
+class DisplayStrategy(AbstractStrategy):
+    """
+    A strategy which display ticks / bars
+    params:
+        n = 10000
+        n_window = 5
+    """
+    def __init__(self, n=100, n_window=5):
+        self.n = n
+        self.n_window = n_window
+        self.i = 0
+
+    def calculate_signals(self, event):
+        if event.type in [EventType.TICK, EventType.BAR]:
+            # Format the event for human display
+            if event.type == EventType.BAR:
+                event.open_price = PriceParser.display(event.open_price)
+                event.high_price = PriceParser.display(event.high_price)
+                event.low_price = PriceParser.display(event.low_price)
+                event.close_price = PriceParser.display(event.close_price)
+                event.adj_close_price = PriceParser.display(event.adj_close_price)
+            else:  # event.type == EventType.TICK
+                event.bid = PriceParser.display(event.bid)
+                event.ask = PriceParser.display(event.ask)
+
+            if self.i % self.n in range(self.n_window):
+                print("%d %s" % (self.i, event))
+            self.i += 1
 
 
 class BuyAndHoldStrategy(AbstractStrategy):
@@ -71,7 +101,8 @@ def run(config, testing, tickers, filename):
     )
 
     # Use the Buy and Hold Strategy
-    strategy = BuyAndHoldStrategy(tickers, events_queue)
+    strategy = Strategies(BuyAndHoldStrategy(tickers, events_queue), DisplayStrategy(n=20))
+
 
     # Start/End TODO redundant -- only required for default (Yahoo) price handler.
     start_date = datetime.datetime(2000, 1, 1)
