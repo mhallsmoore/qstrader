85462ea49c9b6b0e9839cb9d1939bfe1ee790d36
Michael Halls-Moore <mike@quarkgluon.com>
2017-10-12 17:15:11 +0100

    Added back version.py.

---
+++ qstrader/version.py
@@ -0,0 +1,8 @@
+__author__ = "Michael Halls-Moore"
+__copyright__ = "Copyright 2017"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "mhallsmoore"
+__email__ = "support@quantstart.com"
+__status__ = "Development"
+__url__ = "https://github.com/mhallsmoore/qstrader"
