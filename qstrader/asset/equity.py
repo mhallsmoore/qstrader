703637f926e048e71d6ae9d8fb97799649e5bb23
Michael Halls-Moore <mike@quarkgluon.com>
2019-11-06 12:13:33 +0000

    Modified various docstrings, particularly on the SimulatedBroker.

--- qstrader/asset/equity.py
+++ qstrader/asset/equity.py
@@ -7,14 +7,14 @@ class Equity(Asset):
 
     Parameters
     ----------
-    name : str
+    name : `str`
         The asset's name (e.g. the company name and/or
         share class).
-    symbol : str
+    symbol : `str`
         The asset's original ticker symbol.
         TODO: This will require modification to handle proper
         ticker mapping.
-    tax_exempt: boolean, optional
+    tax_exempt: `boolean`, optional
         Is the share exempt from government taxation?
         Necessary for taxation on share transactions, such
         as UK stamp duty.
@@ -32,6 +32,9 @@ class Equity(Asset):
         self.tax_exempt = tax_exempt
 
     def __repr__(self):
+        """
+        String representation of the Equity Asset.
+        """
         return (
             "Equity(name='%s', symbol='%s', tax_exempt=%s)" % (
                 self.name, self.symbol, self.tax_exempt
