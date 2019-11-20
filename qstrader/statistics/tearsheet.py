aefcebe6c58f0eea816d3fd4cbffc3beec88bc1f
Michael Halls-Moore <mike@quarkgluon.com>
2019-11-20 13:48:38 +0000

    Added a benchmark capability to the tearsheet.

--- qstrader/statistics/tearsheet.py
+++ qstrader/statistics/tearsheet.py
@@ -3,7 +3,6 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import matplotlib.dates as mdates
-import pandas as pd
 import numpy as np
 import seaborn as sns
 
@@ -17,31 +16,23 @@ class TearsheetStatistics(Statistics):
     found in institutional strategy performance reports.
     """
     def __init__(
-        self, broker, title=None, periods=252
+        self,
+        strategy_equity,
+        benchmark_equity=None,
+        title=None,
+        periods=252
     ):
-        self.broker = broker
+        self.strategy_equity = strategy_equity
+        self.benchmark_equity = benchmark_equity
         self.title = title
         self.periods = periods
-        self.equity = []
 
-    def update(self, dt):
-        """
-        Update all statistics that must be tracked over time.
-        """
-        self.equity.append(
-            (dt, self.broker.get_account_total_equity()["master"])
-        )
-
-    def get_results(self):
+    def get_results(self, equity_df):
         """
         Return a dict with all important results & stats.
         """
-        # Equity
-        equity_df = pd.DataFrame(self.equity, columns=["timestamp", "equity"])
-        equity_df.set_index("timestamp", inplace=True)
-
         # Returns
-        equity_df["returns"] = equity_df["equity"].pct_change().fillna(0.0)
+        equity_df["returns"] = equity_df["Equity"].pct_change().fillna(0.0)
 
         # Cummulative Returns
         equity_df["cum_returns"] = np.exp(np.log(1 + equity_df["returns"]).cumsum())
@@ -58,19 +49,19 @@ class TearsheetStatistics(Statistics):
         statistics["max_drawdown"] = max_dd
         statistics["max_drawdown_pct"] = max_dd
         statistics["max_drawdown_duration"] = dd_dur
-        statistics["equity"] = equity_df["equity"]
+        statistics["equity"] = equity_df["Equity"]
         statistics["returns"] = equity_df["returns"]
         statistics["cum_returns"] = equity_df["cum_returns"]
         return statistics
 
-    def _plot_equity(self, stats, ax=None, **kwargs):
+    def _plot_equity(self, strat_stats, bench_stats=None, ax=None, **kwargs):
         """
         Plots cumulative rolling returns versus some benchmark.
         """
         def format_two_dec(x, pos):
             return '%.2f' % x
 
-        equity = stats['cum_returns']
+        equity = strat_stats['cum_returns']
 
         if ax is None:
             ax = plt.gca()
@@ -84,7 +75,12 @@ class TearsheetStatistics(Statistics):
         ax.xaxis.grid(linestyle=':')
 
         equity.plot(lw=2, color='green', alpha=0.6, x_compat=False,
-                    label='Backtest', ax=ax, **kwargs)
+                    label='Strategy', ax=ax, **kwargs)
+        if bench_stats is not None:
+            bench_stats['cum_returns'].plot(
+                lw=2, color='gray', alpha=0.6, x_compat=False,
+                label='Benchmark', ax=ax, **kwargs
+            )
 
         ax.axhline(1.0, linestyle='--', color='black', lw=1)
         ax.set_ylabel('Cumulative returns')
@@ -182,17 +178,12 @@ class TearsheetStatistics(Statistics):
 
         return ax
 
-    def _plot_txt_curve(self, stats, ax=None, **kwargs):
+    def _plot_txt_curve(self, stats, bench_stats=None, ax=None, **kwargs):
         """
         Outputs the statistics for the equity curve.
         """
         def format_perc(x, pos):
             return '%.0f%%' % x
-
-        returns = stats["returns"]
-        cum_returns = stats['cum_returns']
-
-        trd_yr = 0
 
         if ax is None:
             ax = plt.gca()
@@ -200,39 +191,61 @@ class TearsheetStatistics(Statistics):
         y_axis_formatter = FuncFormatter(format_perc)
         ax.yaxis.set_major_formatter(FuncFormatter(y_axis_formatter))
 
+        # Strategy statistics
+        returns = stats["returns"]
+        cum_returns = stats['cum_returns']
         tot_ret = cum_returns[-1] - 1.0
         cagr = perf.create_cagr(cum_returns, self.periods)
         sharpe = perf.create_sharpe_ratio(returns, self.periods)
         sortino = perf.create_sortino_ratio(returns, self.periods)
         dd, dd_max, dd_dur = perf.create_drawdowns(cum_returns)
 
-        ax.text(0.25, 8.9, 'Total Return', fontsize=8)
-        ax.text(7.50, 8.9, '{:.0%}'.format(tot_ret), fontweight='bold', horizontalalignment='right', fontsize=8)
+        # Benchmark statistics
+        if bench_stats is not None:
+            bench_returns = bench_stats["returns"]
+            bench_cum_returns = bench_stats['cum_returns']
+            bench_tot_ret = bench_cum_returns[-1] - 1.0
+            bench_cagr = perf.create_cagr(bench_cum_returns, self.periods)
+            bench_sharpe = perf.create_sharpe_ratio(bench_returns, self.periods)
+            bench_sortino = perf.create_sortino_ratio(bench_returns, self.periods)
+            bench_dd, bench_dd_max, bench_dd_dur = perf.create_drawdowns(bench_cum_returns)
 
-        ax.text(0.25, 7.9, 'CAGR', fontsize=8)
-        ax.text(7.50, 7.9, '{:.2%}'.format(cagr), fontweight='bold', horizontalalignment='right', fontsize=8)
+        # Strategy Values
+        ax.text(7.50, 8.2, 'Strategy', fontweight='bold', horizontalalignment='right', fontsize=8, color='green')
 
-        ax.text(0.25, 6.9, 'Sharpe Ratio', fontsize=8)
-        ax.text(7.50, 6.9, '{:.2f}'.format(sharpe), fontweight='bold', horizontalalignment='right', fontsize=8)
+        ax.text(0.25, 6.9, 'Total Return', fontsize=8)
+        ax.text(7.50, 6.9, '{:.0%}'.format(tot_ret), fontweight='bold', horizontalalignment='right', fontsize=8)
 
-        ax.text(0.25, 5.9, 'Sortino Ratio', fontsize=8)
-        ax.text(7.50, 5.9, '{:.2f}'.format(sortino), fontweight='bold', horizontalalignment='right', fontsize=8)
+        ax.text(0.25, 5.9, 'CAGR', fontsize=8)
+        ax.text(7.50, 5.9, '{:.2%}'.format(cagr), fontweight='bold', horizontalalignment='right', fontsize=8)
 
-        ax.text(0.25, 4.9, 'Annual Volatility', fontsize=8)
-        ax.text(7.50, 4.9, '{:.2%}'.format(returns.std() * np.sqrt(252)), fontweight='bold', horizontalalignment='right', fontsize=8)
+        ax.text(0.25, 4.9, 'Sharpe Ratio', fontsize=8)
+        ax.text(7.50, 4.9, '{:.2f}'.format(sharpe), fontweight='bold', horizontalalignment='right', fontsize=8)
 
-        ax.text(0.25, 3.9, 'R-Squared', fontsize=8)
-        ax.text(7.50, 3.9, 'N/A', fontweight='bold', horizontalalignment='right', fontsize=8)
+        ax.text(0.25, 3.9, 'Sortino Ratio', fontsize=8)
+        ax.text(7.50, 3.9, '{:.2f}'.format(sortino), fontweight='bold', horizontalalignment='right', fontsize=8)
 
-        ax.text(0.25, 2.9, 'Max Daily Drawdown', fontsize=8)
-        ax.text(7.50, 2.9, '{:.2%}'.format(dd_max), color='red', fontweight='bold', horizontalalignment='right', fontsize=8)
+        ax.text(0.25, 2.9, 'Annual Volatility', fontsize=8)
+        ax.text(7.50, 2.9, '{:.2%}'.format(returns.std() * np.sqrt(252)), fontweight='bold', horizontalalignment='right', fontsize=8)
 
-        ax.text(0.25, 1.9, 'Max Drawdown Duration', fontsize=8)
-        ax.text(7.50, 1.9, '{:.0f}'.format(dd_dur), fontweight='bold', horizontalalignment='right', fontsize=8)
+        ax.text(0.25, 1.9, 'Max Daily Drawdown', fontsize=8)
+        ax.text(7.50, 1.9, '{:.2%}'.format(dd_max), color='red', fontweight='bold', horizontalalignment='right', fontsize=8)
 
-        ax.text(0.25, 0.9, 'Trades per Year', fontsize=8)
-        ax.text(7.50, 0.9, '{:.1f}'.format(trd_yr), fontweight='bold', horizontalalignment='right', fontsize=8)
-        ax.set_title('Curve', fontweight='bold')
+        ax.text(0.25, 0.9, 'Max Drawdown Duration (Days)', fontsize=8)
+        ax.text(7.50, 0.9, '{:.0f}'.format(dd_dur), fontweight='bold', horizontalalignment='right', fontsize=8)
+
+        # Benchmark Values
+        if bench_stats is not None:
+            ax.text(10.0, 8.2, 'Benchmark', fontweight='bold', horizontalalignment='right', fontsize=8, color='gray')
+            ax.text(10.0, 6.9, '{:.0%}'.format(bench_tot_ret), fontweight='bold', horizontalalignment='right', fontsize=8)
+            ax.text(10.0, 5.9, '{:.2%}'.format(bench_cagr), fontweight='bold', horizontalalignment='right', fontsize=8)
+            ax.text(10.0, 4.9, '{:.2f}'.format(bench_sharpe), fontweight='bold', horizontalalignment='right', fontsize=8)
+            ax.text(10.0, 3.9, '{:.2f}'.format(bench_sortino), fontweight='bold', horizontalalignment='right', fontsize=8)
+            ax.text(10.0, 2.9, '{:.2%}'.format(bench_returns.std() * np.sqrt(252)), fontweight='bold', horizontalalignment='right', fontsize=8)
+            ax.text(10.0, 1.9, '{:.2%}'.format(bench_dd_max), color='red', fontweight='bold', horizontalalignment='right', fontsize=8)
+            ax.text(10.0, 0.9, '{:.0f}'.format(bench_dd_dur), fontweight='bold', horizontalalignment='right', fontsize=8)
+
+        ax.set_title('Equity Curve', fontweight='bold')
 
         ax.grid(False)
         ax.spines['top'].set_linewidth(2.0)
@@ -251,6 +264,7 @@ class TearsheetStatistics(Statistics):
         """
         Plot the Tearsheet
         """
+        ""
         rc = {
             'lines.linewidth': 1.0,
             'axes.facecolor': '0.995',
@@ -272,11 +286,15 @@ class TearsheetStatistics(Statistics):
         sns.set_palette("deep", desat=.6)
 
         vertical_sections = 5
-        fig = plt.figure(figsize=(10, 12))
+        fig = plt.figure(figsize=(16, 12))
         fig.suptitle(self.title, y=0.94, weight='bold')
         gs = gridspec.GridSpec(vertical_sections, 3, wspace=0.25, hspace=0.5)
 
-        stats = self.get_results()
+        stats = self.get_results(self.strategy_equity)
+        bench_stats = None
+        if self.benchmark_equity is not None:
+            bench_stats = self.get_results(self.benchmark_equity)
+
         ax_equity = plt.subplot(gs[:2, :])
         ax_drawdown = plt.subplot(gs[2, :])
         ax_monthly_returns = plt.subplot(gs[3, :2])
@@ -285,11 +303,11 @@ class TearsheetStatistics(Statistics):
         # ax_txt_trade = plt.subplot(gs[4, 1])
         # ax_txt_time = plt.subplot(gs[4, 2])
 
-        self._plot_equity(stats, ax=ax_equity)
+        self._plot_equity(stats, bench_stats=bench_stats, ax=ax_equity)
         self._plot_drawdown(stats, ax=ax_drawdown)
         self._plot_monthly_returns(stats, ax=ax_monthly_returns)
         self._plot_yearly_returns(stats, ax=ax_yearly_returns)
-        self._plot_txt_curve(stats, ax=ax_txt_curve)
+        self._plot_txt_curve(stats, bench_stats=bench_stats, ax=ax_txt_curve)
         # self._plot_txt_trade(stats, ax=ax_txt_trade)
         # self._plot_txt_time(stats, ax=ax_txt_time)
 
