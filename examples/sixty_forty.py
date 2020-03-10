d3e1ccc47c01f14ef923cacce8dd521683f9c3dd
Michael Halls-Moore <mike@quarkgluon.com>
2020-03-10 13:02:15 +0000

    Fixed Buy & Hold and 60/40 Monthly Rebalance examples to work with new API.

--- examples/sixty_forty.py
+++ examples/sixty_forty.py
@@ -1,47 +1,63 @@
+import os
+
 import pandas as pd
 import pytz
 
 from qstrader.alpha_model.fixed_signals import FixedSignalsAlphaModel
+from qstrader.asset.equity import Equity
+from qstrader.asset.universe.static import StaticUniverse
+from qstrader.data.backtest_data_handler import BacktestDataHandler
+from qstrader.data.daily_bar_csv import CSVDailyBarDataSource
 from qstrader.statistics.tearsheet import TearsheetStatistics
 from qstrader.trading.backtest import BacktestTradingSession
 
 
 if __name__ == "__main__":
-    start_dt = pd.Timestamp('2004-01-01 00:00:00', tz=pytz.UTC)
-    end_dt = pd.Timestamp('2018-12-31 23:59:00', tz=pytz.UTC)
+    start_dt = pd.Timestamp('2003-09-30 14:30:00', tz=pytz.UTC)
+    end_dt = pd.Timestamp('2019-12-31 23:59:00', tz=pytz.UTC)
 
-    # Strategic Asset Allocation - Fixed Weight 60/40 SPY/AGG
-    strategy_assets = ['EQ:SPY', 'EQ:AGG']
-    strategy_signal_weights = {'EQ:SPY': 0.6, 'EQ:AGG': 0.4}
-    strategy_title = 'Strategic Asset Allocation - 60/40 US Equities/Bonds (SPY/AGG)'
-    strategy_alpha_model = FixedSignalsAlphaModel(strategy_signal_weights)
+    # Construct the symbols and assets necessary for the backtest
+    strategy_symbols = ['SPY', 'AGG']
+    strategy_assets = ['EQ:%s' % symbol for symbol in strategy_symbols]
+    strategy_universe = StaticUniverse(strategy_assets)
+
+    # To avoid loading all CSV files in the directory, set the
+    # data source to load only those provided symbols
+    csv_dir = os.environ.get('QSTRADER_CSV_DATA_DIR')
+    data_source = CSVDailyBarDataSource(csv_dir, Equity, csv_symbols=strategy_symbols)
+    data_handler = BacktestDataHandler(strategy_universe, data_sources=[data_source])
+
+    # Construct an Alpha Model that simply provides
+    # static allocations to a universe of assets
+    # In this case 60% SPY ETF, 40% AGG ETF,
+    # rebalanced at the end of each month
+    strategy_alpha_model = FixedSignalsAlphaModel({'EQ:SPY': 0.6, 'EQ:AGG': 0.4})
     strategy_backtest = BacktestTradingSession(
         start_dt,
         end_dt,
-        strategy_assets,
+        strategy_universe,
         strategy_alpha_model,
         rebalance='end_of_month',
-        account_name='Strategic Asset Allocation Account',
-        portfolio_id='SAA001',
-        portfolio_name=strategy_title,
-        cash_buffer_percentage=0.05
+        cash_buffer_percentage=0.01,
+        data_handler=data_handler
     )
     strategy_backtest.run()
 
-    # Benchmark - Buy & Hold SPY
+    # Construct benchmark assets (buy & hold SPY)
     benchmark_assets = ['EQ:SPY']
-    benchmark_signal_weights = {'EQ:SPY': 1.0}
-    benchmark_alpha_model = FixedSignalsAlphaModel(benchmark_signal_weights)
+    benchmark_universe = StaticUniverse(benchmark_assets)
+
+    # Construct a benchmark Alpha Model that provides
+    # 100% static allocation to the SPY ETF, with no rebalance
+    benchmark_alpha_model = FixedSignalsAlphaModel({'EQ:SPY': 1.0})
     benchmark_backtest = BacktestTradingSession(
         start_dt,
         end_dt,
-        benchmark_assets,
+        benchmark_universe,
         benchmark_alpha_model,
-        account_name='Benchmark Account',
-        portfolio_id='SPY001',
-        portfolio_name='Benchmark - Buy & Hold S&P500 (SPY)',
         rebalance='buy_and_hold',
-        cash_buffer_percentage=0.05
+        cash_buffer_percentage=0.01,
+        data_handler=data_handler
     )
     benchmark_backtest.run()
 
@@ -49,6 +65,6 @@ if __name__ == "__main__":
     tearsheet = TearsheetStatistics(
         strategy_equity=strategy_backtest.get_equity_curve(),
         benchmark_equity=benchmark_backtest.get_equity_curve(),
-        title=strategy_title
+        title='60/40 US Equities/Bonds'
     )
     tearsheet.plot_results()
