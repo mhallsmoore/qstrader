165ce9be5f78f6c0a23251186cf3e4637ac7297f
Michael Halls-Moore <mike@quarkgluon.com>
2020-06-24 11:20:45 +0100

    Added basic integration test for long/short leveraged portfolio.

--- tests/integration/trading/test_backtest_e2e.py
+++ tests/integration/trading/test_backtest_e2e.py
@@ -8,7 +8,7 @@ from qstrader.asset.universe.static import StaticUniverse
 from qstrader.trading.backtest import BacktestTradingSession
 
 
-def test_backtest_sixty_forty_no_corp_actions(etf_filepath):
+def test_backtest_sixty_forty(etf_filepath):
     """
     Ensures that a full end-to-end weekly rebalanced backtested
     trading session with fixed proportion weights produces the
@@ -60,7 +60,64 @@ def test_backtest_sixty_forty_no_corp_actions(etf_filepath):
     }
 
     history_df = portfolio.history_to_df().reset_index()
-    expected_df = pd.read_csv(os.path.join(etf_filepath, 'history.dat'))
+    expected_df = pd.read_csv(os.path.join(etf_filepath, 'sixty_forty_history.dat'))
+
+    pd.testing.assert_frame_equal(history_df, expected_df)
+    assert portfolio_dict == expected_dict
+
+
+def test_backtest_long_short_leveraged(etf_filepath):
+    """
+    Ensures that a full end-to-end daily rebalanced backtested
+    trading session of a leveraged long short portfolio with
+    fixed proportion weights produces the correct rebalance
+    orders as well as correctly calculated market values after
+    a single month's worth of daily backtesting.
+    """
+    os.environ['QSTRADER_CSV_DATA_DIR'] = etf_filepath
+
+    assets = ['EQ:ABC', 'EQ:DEF']
+    universe = StaticUniverse(assets)
+    signal_weights = {'EQ:ABC': 1.0, 'EQ:DEF': -0.7}
+    alpha_model = FixedSignalsAlphaModel(signal_weights)
+
+    start_dt = pd.Timestamp('2019-01-01 00:00:00', tz=pytz.UTC)
+    end_dt = pd.Timestamp('2019-01-31 23:59:00', tz=pytz.UTC)
+
+    backtest = BacktestTradingSession(
+        start_dt,
+        end_dt,
+        universe,
+        alpha_model,
+        portfolio_id='000001',
+        rebalance='daily',
+        long_only=False,
+        gross_leverage=2.0
+    )
+    backtest.run(results=False)
+
+    portfolio = backtest.broker.portfolios['000001']
+
+    portfolio_dict = portfolio.portfolio_to_dict()
+    expected_dict = {
+        'EQ:ABC': {
+            'unrealised_pnl': -48302.832839363175,
+            'realised_pnl': -3930.9847615026706,
+            'total_pnl': -52233.81760086585,
+            'market_value': 1055344.698660986,
+            'quantity': 8782.0
+        },
+        'EQ:DEF': {
+            'unrealised_pnl': -42274.737165376326,
+            'realised_pnl': -9972.897320721153,
+            'total_pnl': -52247.63448609748,
+            'market_value': -742417.5692312752,
+            'quantity': -2824.0
+        }
+    }
+
+    history_df = portfolio.history_to_df().reset_index()
+    expected_df = pd.read_csv(os.path.join(etf_filepath, 'long_short_history.dat'))
 
     pd.testing.assert_frame_equal(history_df, expected_df)
     assert portfolio_dict == expected_dict
