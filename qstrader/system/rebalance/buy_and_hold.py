6f9f0a270fd6b3cc41de94f8c80541df72e1dde1
Juliette James <juliettemariejames@gmail.com>
2024-04-25 19:48:29 +0100

    Updated rebalance buy and hold to check for whether backtest start date is a business day

--- qstrader/system/rebalance/buy_and_hold.py
+++ qstrader/system/rebalance/buy_and_hold.py
@@ -1,10 +1,12 @@
+import pandas as pd
+from pandas.tseries.offsets import BusinessDay
 from qstrader.system.rebalance.rebalance import Rebalance
 
 
 class BuyAndHoldRebalance(Rebalance):
     """
-    Generates a single rebalance timestamp at the start date in
-    order to create a single set of orders at the beginning of
+    Generates a single rebalance timestamp at the first business day 
+    after the start date. Creates a single set of orders at the beginning of
     a backtest, with no further rebalances carried out.
 
     Parameters
@@ -15,4 +17,32 @@ class BuyAndHoldRebalance(Rebalance):
 
     def __init__(self, start_dt):
         self.start_dt = start_dt
-        self.rebalances = [start_dt]
+        self.rebalances = self._generate_rebalances()
+
+    def _is_business_day(self):
+        """
+        Checks if the start_dt is a business day.
+        
+        Returns
+        -------
+        `boolean`
+        """
+        return bool(len(pd.bdate_range(self.start_dt, self.start_dt)))
+
+    def _generate_rebalances(self):
+        """
+        Outputs the rebalance timestamp offset to the next
+        business day.
+
+        Does not include holidays.
+
+        Returns
+        -------
+        `list[pd.Timestamp]`
+            The rebalance timestamp list.
+        """
+        if not self._is_business_day():
+            rebalance_date = self.start_dt + BusinessDay()
+        else:
+            rebalance_date = self.start_dt
+        return [rebalance_date]
